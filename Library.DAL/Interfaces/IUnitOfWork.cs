1b4e05a6a9d38b7fc09fc30966c12d16bd1a5a32
ejen2008 <ejen2008@mail.ru>
2017-09-05 23:17:57 +0300

    Add

---
+++ Library.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Library.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library.DAL.Interfaces
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        IRepository<Book> Books { get; }
+        IRepository<Author> Authors { get; }
+        void Save();
+    }
+}
---
+++ Library.DAL/Repositories/EFUnitOfWork.cs
@@ -0,0 +1,74 @@
+﻿using Library.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Library.DAL.Entities;
+using Library.DAL.EF;
+
+namespace Library.DAL.Repositories
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private LibraryContext _db;
+        private BookRepository _bookRepository;
+        private AuthorRepository _authorRepository;
+        private bool _disposed = false;
+
+        public EFUnitOfWork(string connectionString)
+        {
+            _db = new LibraryContext(connectionString);
+        }
+
+        public IRepository<Author> Authors
+        {
+            get
+            {
+                if (_authorRepository == null)
+                {
+                    _authorRepository = new AuthorRepository(_db);
+                }
+                return _authorRepository;
+            }
+        }
+
+        public IRepository<Book> Books
+        {
+            get
+            {
+                if (_bookRepository == null)
+                {
+                    _bookRepository = new BookRepository(_db);
+                }
+                return _bookRepository;
+            }
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                IfNotDisposed(disposing);
+                _disposed = true;
+            }
+        }
+        private void IfNotDisposed(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
